5a24e7f10b53e248df582ace4398231a0e3ca232
sagiahrac <sagiahrak@gmail.com>
2025-08-12 00:01:02 +0300

    done

--- src/three_ideas_intersection_slide.py
+++ src/three_ideas_intersection_slide.py
@@ -7,11 +7,11 @@ class ThreeIdeasIntersectionSlide(Scene, DDPMBaseMixin):
         
         # Central DDPM
         ddpm_text = Text("DDPM", font_size=48, color=YELLOW, weight=BOLD)
-        ddpm_text.move_to(ORIGIN)
+        ddpm_text.move_to(ORIGIN + 0.5 * DOWN)
         
         # Create a hexagon around DDPM for emphasis
         ddpm_background = RegularPolygon(6, radius=1.2, color=YELLOW, fill_opacity=0.1, stroke_width=3)
-        ddpm_background.move_to(ORIGIN)
+        ddpm_background.move_to(ORIGIN + 0.5 * DOWN)
         
         # Position the three ideas around DDPM
         # Variational Inference - top
@@ -19,7 +19,7 @@ class ThreeIdeasIntersectionSlide(Scene, DDPMBaseMixin):
         vi_text = Paragraph("Variational\nInference", font_size=24, color=BLUE, weight=BOLD, alignment="center")
         vi_text.move_to(vi_box.get_center())
         vi_group = VGroup(vi_box, vi_text)
-        vi_group.move_to(UP * 3)
+        vi_group.move_to(UP * 2)
         
         # ELBO - bottom left
         elbo_box = RoundedRectangle(width=2.5, height=1.25, corner_radius=0.3, color=GREEN, fill_opacity=0.2, stroke_width=2)
@@ -35,30 +35,32 @@ class ThreeIdeasIntersectionSlide(Scene, DDPMBaseMixin):
         score_group = VGroup(score_box, score_text)
         score_group.move_to(DOWN * 2 + RIGHT * 3)
         
-        # Create arrows pointing to DDPM
+        # Create arrows pointing TO DDPM - three ideas converging
         arrow_vi = Arrow(
-            start=vi_group.get_bottom(),
-            end=ddpm_background.get_center() + UP * 0.5,
+            start=vi_group.get_bottom() + 0.15 * UP,
+            end=ddpm_background.get_top() + 0.15 * DOWN,
             color=BLUE,
             stroke_width=4,
-            max_tip_length_to_length_ratio=0.1
+            max_tip_length_to_length_ratio=0.4
         )
         
         arrow_elbo = Arrow(
             start=elbo_group.get_right(),
-            end=ddpm_background.get_center() + LEFT * 0.5 + DOWN * 0.3,
+            end=(ddpm_background.get_left() + ddpm_background.get_bottom()) / 2,
             color=GREEN,
             stroke_width=4,
-            max_tip_length_to_length_ratio=0.1
+            max_tip_length_to_length_ratio=0.25
         )
+        arrow_elbo.shift(LEFT * 0.1)
         
         arrow_score = Arrow(
             start=score_group.get_left(),
-            end=ddpm_background.get_center() + RIGHT * 0.5 + DOWN * 0.3,
+            end=(ddpm_background.get_right() + ddpm_background.get_bottom()) / 2,
             color=RED,
             stroke_width=4,
-            max_tip_length_to_length_ratio=0.1
+            max_tip_length_to_length_ratio=0.25
         )
+        arrow_score.shift(RIGHT * 0.1)
         
         # Animate the three ideas appearing
         self.play(
@@ -98,6 +100,12 @@ class ThreeIdeasIntersectionSlide(Scene, DDPMBaseMixin):
         # Final emphasis
         self.play(
             ddpm_text.animate.scale(1.2),
+            ddpm_background.animate.set_stroke(width=5),
+            run_time=1
+        )
+
+        self.play(
+            ddpm_text.animate.scale(1 / 1.2),
             ddpm_background.animate.set_stroke(width=5),
             run_time=1
         )
